aa93f8dbbc71586a02c371a50dc62b4d689d587c
Jared Hester <jared.hester@gmail.com>
2016-08-05 15:25:07 -0400

    setup build process

---
+++ build.cmd
@@ -0,0 +1,14 @@
+﻿@echo off
+cls
+
+.paket\paket.bootstrapper.exe 
+if errorlevel 1 (
+  exit /b %errorlevel%
+)
+
+.paket\paket.exe restore
+if errorlevel 1 (
+  exit /b %errorlevel%
+)
+
+packages\build\FAKE\tools\FAKE.exe build.fsx %* --nocache
---
+++ build.fsx
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------------
+// FAKE build script
+// --------------------------------------------------------------------------------------
+
+#I "packages/build/FAKE/tools"
+#r "FakeLib.dll"
+open System
+open System.Diagnostics
+open System.IO
+open Fake
+
+let run cmd args dir =
+    if execProcess( fun info ->
+        info.FileName <- cmd
+        if not( String.IsNullOrWhiteSpace dir) then
+            info.WorkingDirectory <- dir
+        info.Arguments <- args
+    ) System.TimeSpan.MaxValue = false then
+        traceError <| sprintf "Error while running '%s' with args: %s" cmd args
+
+let platformTool tool path =
+    isUnix |> function | true -> tool | _ -> path
+
+let npmTool =
+    platformTool "npm" ("packages"</>"build"</>"Npm.js"</>"tools"</>"npm.cmd" |> FullName)
+
+Target "RunScript" (fun () ->
+    run npmTool "install" "release"
+    run npmTool "run build" "release"
+)
+
+
+RunTargetOrDefault "RunScript"
