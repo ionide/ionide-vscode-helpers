575c08c7957e97dadd7d4be84114575c7573f9e1
BooksBaum <15612932+Booksbaum@users.noreply.github.com>
2020-09-06 14:34:28 +0200

    Fix Helpers
    Remove Promise module -> Fable.Promise

--- src/Helpers.fs
+++ src/Helpers.fs
@@ -8,106 +8,6 @@ module JS =
     [<Emit("($0 != undefined)")>]
     let isDefined (o: obj) : bool = failwith "never"
 
-//---------------------------------------------------
-//PromisesExt (by Dave)
-//---------------------------------------------------
-module Promise =
-    open System
-    open Fable.Core
-    open Fable.Import
-    open Fable.Import.JS
-
-    /// <summary>
-    /// Creates new promise.
-    /// Constuctor function "body" is called with two arguments:
-    /// "resolve" - moves promise to fulfilled state, using provided argument as result
-    /// "reject" - moves promise to rejected state
-    /// </summary>
-    let create (body: ('T->unit) -> (obj->unit) -> unit) =
-        Promise.Create(fun (resolverFunc : Func<U2<_, _>, _>) (rejectorFunc : Func<_,_>) ->
-            body (fun v -> resolverFunc.Invoke(U2.Case1 v)) (fun e -> rejectorFunc.Invoke e))
-
-    /// <summary>
-    /// Standard map implementation.
-    /// </summary>
-    let map (a : 'T -> 'R) (pr : Promise<'T>) : Promise<'R> =
-        pr.``then``(
-            unbox<Func<'T, U2<'R, PromiseLike<'R>>>> a,
-            unbox<Func<obj,unit>> None
-        )
-
-    /// <summary>
-    /// Standard bind implementation.
-    /// </summary>
-    let bind (a : 'T -> Promise<'R>) (pr : Promise<'T>) : Promise<'R> =
-        pr.``then``(
-            unbox<Func<'T, U2<'R, PromiseLike<'R>>>> a,
-            unbox<Func<obj,unit>> None
-        )
-
-    /// <summary>
-    /// Bind for rejected promise.
-    /// </summary>
-    let catch (a : obj -> Promise<'R>) (pr : Promise<'T>) : Promise<'R> =
-        pr.``then``(
-            unbox<Func<'T, U2<'R, PromiseLike<'R>>>> None,
-            unbox<Func<obj,U2<'R, PromiseLike<'R>>>> a
-        )
-
-    /// <summary>
-    /// Combination of bind and catch methods. If promise in fulfilled state - a is bound, if in rejected state - then b.
-    /// </summary>
-    let either (a : 'T -> Promise<'R>) (b: obj -> Promise<'R>) (pr : Promise<'T>) : Promise<'R> =
-        pr.``then``(
-            unbox<Func<'T, U2<'R, PromiseLike<'R>>>> a,
-            unbox<Func<obj,U2<'R, PromiseLike<'R>>>> b
-        )
-
-    /// <summary>
-    /// Creates promise (in pending state) from the supplied value.
-    /// </summary>
-    let lift<'T> (a : 'T) : Promise<'T> =
-        Promise.resolve(U2.Case1 a)
-
-    /// <summary>
-    /// Creates promise (in rejected state) with supplied reason.
-    /// </summary>
-    let reject<'T> reason : Promise<'T> =
-        Promise.reject<'T> reason
-
-    /// <summary>
-    /// Allows handing promise which is in fulfilled state.
-    /// Can be used for side-effects.
-    /// </summary>
-    let onSuccess (a : 'T -> unit) (pr : Promise<'T>) : Promise<'T> =
-        pr.``then``(
-            unbox<Func<'T, U2<'T, PromiseLike<'T>>>> (fun value -> a value; value),
-            unbox<Func<obj,unit>> None
-        )
-
-    /// <summary>
-    /// Allows handing promise which is in rejected state. Propagates rejected promise, to allow chaining.
-    /// Can be used for side-effects.
-    /// </summary>
-    let onFail (a : obj -> unit) (pr : Promise<'T>) : Promise<'T> =
-        pr.catch (unbox<Func<obj, U2<'T, PromiseLike<'T>>>> (fun reason -> a reason |> ignore; reject reason))
-
-    let all (prs : Promise<'T> seq): JS.Promise<ResizeArray<'T>> =
-        Promise.all (unbox prs)
-
-    let empty<'T> = lift (unbox<'T>null)
-
-    type PromiseBuilder() =
-        member inline x.Bind(m,f) = bind f m
-        member inline x.Return(a) = lift a
-        member inline x.ReturnFrom(a) = a
-        member inline x.Zero() = Fable.Import.JS.Promise.resolve()
-
-[<AutoOpen>]
-module PromiseBuilderImp =
-    let promise = Promise.PromiseBuilder()
-
-
 
 //---------------------------------------------------
 //VS Code Helpers
@@ -144,13 +44,12 @@ module Process =
             |> List.rev
             |> List.toArray
 
-    open Fable.Import.JS
-    open Fable.Import.Node
-    open Fable.Import.Node.ChildProcess
+    open Node
+    open Node.ChildProcess
     open Fable.Import.vscode
     open Fable.Core.JsInterop
 
-    module node = Fable.Import.Node.Exports
+    module node = Node.Api
 
     [<Emit("process.platform")>]
     let private platform: string = jsNative
@@ -235,18 +134,18 @@ module Process =
         Signal: string option
     }
 
-    let toPromise (proc : ChildProcess) =
-        Promise.Create<ChildProcessExit>(fun (resolve : Func<U2<ChildProcessExit,PromiseLike<ChildProcessExit>>,unit>) (_error : Func<obj,_>) ->
+    let toPromise (proc : ChildProcess): JS.Promise<ChildProcessExit> =
+        Promise.create (fun resolve _ -> 
             proc
             |> onExit(fun code signal -> { Code = code; Signal = signal } |> Case1 |> unbox resolve )
             |> ignore
         )
 
-    let exec location linuxCmd cmd : Promise<ExecError option * string * string> =
+    let exec location linuxCmd cmd : JS.Promise<ExecError option * string * string> =
         let options = createEmpty<ExecOptions>
         options.cwd <- Some workspace.rootPath
-
-        Promise.Create<ExecError option * string * string>(fun resolve error ->
+        
+        Promise.create (fun resolve error ->
             let execCmd =
                 if isWin () then location + " " + cmd
                 else linuxCmd + " " + location + " " + cmd
@@ -255,7 +154,9 @@ module Process =
                     // As we don't specify an encoding, the documentation specifies that we'll receive strings
                     // "By default, Node.js will decode the output as UTF-8 and pass strings to the callback"
                     let arg = e, unbox<string> i, unbox<string> o
-                    resolve.Invoke(U2.Case1 arg))) |> ignore)
+                    resolve arg)
+            ) |> ignore
+        )
 
 
 //---------------------------------------------------
@@ -263,8 +164,8 @@ module Process =
 //---------------------------------------------------
 module Settings =
     open Fable.Import.vscode
-    open Fable.Import.Node
-    module node =  Fable.Import.Node.Exports
+    open Node
+    module node =  Node.Api
 
     module Toml =
         [<Emit("toml.parse($0)")>]
@@ -298,7 +199,7 @@ module Settings =
     let loadOrDefault<'a> (map : Settings -> 'a)  (def :'a) =
         try
             let path = workspace.rootPath + "/.ionide"
-            let t = node.fs.readFileSync(path).toString ()
+            let t = node.fs.readFileSync(path).ToString ()
                     |> Toml.parse
                     |> map
             if JS.isDefined t then t else def
